e6ffa775e5423b8f2b7c4020e275bc682323c0b3
박병진 <pijoh3@naver.com>
2023-02-17 00:16:45 +0900

    index.html 메타 태그, font, favicon 설정, 기본 컴포넌트 구조 구성

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app">
+  <TodoHeader/>
+  <TodoInput/>
+  <TodoList/>
+  <TodoFooter/>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoHeader from "./components/TodoHeader.vue"
+import TodoInput from "./components/TodoInput.vue"
+import TodoList from "./components/TodoList.vue"
+import TodoFooter from "./components/TodoFooter.vue"
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    'TodoHeader':TodoHeader,
+    'TodoInput': TodoInput,
+    'TodoList': TodoList,
+    'TodoFooter': TodoFooter
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
