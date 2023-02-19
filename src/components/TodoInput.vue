8f42937e15617096724be854c076cac0e12f6749
박병진 <pijoh3@naver.com>
2023-02-20 00:48:00 +0900

    todoHeader, todoInput, TodoList 초기 작업(TodoList 보완작업 필요)

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,15 +1,54 @@
 <template>
-    <div>
-        input
+    <div class="inputBox shadow">
+        <input type="text" v-model="newTodoItem" @keyup.enter="addTodo">
+        <span class="addContainer">
+            <i class="fa-solid fa-plus addBtn" @click="addTodo"></i>
+        </span>
     </div>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            newTodoItem: ""
+        }
+    },
+    methods: {
+        addTodo() {
+            localStorage.setItem(this.newTodoItem,this.newTodoItem)
+            this.clearInput()
+        },
+        clearInput() {
+            this.newTodoItem = ''
+        }
+    }
 }
 </script>
 
-<style>
-
+<style scoped>
+input:focus {
+    outline: none;
+}
+.inputBox {
+    background: white;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+}
+.inputBox input {
+    border-style: none;
+    font-size: 0.9rem;
+}
+.addContainer {
+    float: right;
+    background: linear-gradient(to right, #6478FB, #8763FB);
+    display: block;
+    width: 3rem;
+    border-radius: 0 5px 5px 0;
+}
+.addBtn {
+    color: white;
+    vertical-align: middle;
+}
 </style>
