ff5bd8a06d189438f80c721c3d048c93114e9e64
Park Byeoung Jin <pijoh3@naver.com>
2023-03-07 14:28:34 +0900

    라우터로 todo 앱 이동 구현

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,13 +1,14 @@
 <template>
   <h1>Vue Project</h1>
-  <VCard :overLine="'basic vue'" :title="'Todo'" :content="contentTemplate"/>
+  <VCard :overLine="'basic vue'" :title="'Todo'" :content="todoTemplate" @click="$event=>$router.push('/todo')"/>
 </template>
 
 <script setup lang="ts">
 import VCard from '@/components/common/VCard.vue';
 
-const contentTemplate = `<p>Option API와 Composition API를 Todo 앱 예제를 통해 비교해봅니다.</p>
+const todoTemplate = `<p>Option API와 Composition API를 Todo 앱 예제를 통해 비교해봅니다.</p>
                          <p><b>사용 기술</b> : Typescript, Option API, Composition API</p>`
+
 </script>
 
 <style lang="scss" scoped>
