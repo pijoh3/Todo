1afe2363ddc568fe87c28008975dad14343b30c8
Park Byeoung Jin <pijoh3@naver.com>
2023-03-13 10:30:43 +0900

    아코디언 컴포넌트 구현

--- src/components/common/VCheckboxAccordion.vue
+++ src/components/common/VCheckboxAccordion.vue
@@ -1,25 +1,13 @@
 <template>
   <v-expansion-panels class="VCheckboxAccordion">
     <v-expansion-panel>
-      <v-expansion-panel-title class="VCheckboxAccordion__label__headline">
-        <div>
-          <v-checkbox>
-            <template #label>
-              <v-label style="{$label-font-size : 40rem; }" :text="'전체동의'" />
-            </template>
-          </v-checkbox>
-        </div>
+      <v-expansion-panel-title v-model="_modelValue" class="VCheckboxAccordion__label__headline">
+        <v-checkbox :label="titleText" v-model="_isChecked" @click.stop @change="onClickCheckAll($event, _isChecked)" :true-icon="'mdi-check-circle'" :false-icon="'mdi-check-circle-outline'">
+        </v-checkbox>
       </v-expansion-panel-title>
-      <v-expansion-panel-text class="VcheckboxAccordion__content">
-        <div class="agreeCheck__item">
-          <v-checkbox :label="'[필수]선택입니다.'" />
-          본 약관에 동의?
-        </div>
-      </v-expansion-panel-text>
-      <v-expansion-panel-text>
-        <div class="agreeCheck__item">
-          <v-checkbox :label="'[필수]선택입2니다.'" />
-            본 약관에 보감?
+      <v-expansion-panel-text v-for="(item,idx) in _contents.contentArr" :key="item.id">
+        <v-checkbox :label="item.checkboxLabel" v-model="item.checked" @change="onClickCheck($event, idx, item.checked, item.id)"/>
+        <div class="item__detail" v-html="item.detail">
         </div>
       </v-expansion-panel-text>
     </v-expansion-panel>
@@ -27,7 +15,84 @@
 </template>
 
 <script setup lang="ts">
+import {reactive, ref, watch} from "vue"
 
+// interface
+export interface VCheckboxAccordionContentProps {
+  id?: string
+  checkboxLabel: string
+  checked: boolean
+  detail?: HTMLElement | string
+}
+
+export interface VCheckboxAccordionProps {
+  id?: string
+  name?: string
+  titleText: string // 아코디언 타이틀
+  modelValue?: boolean // 아코디언 오픈 여부
+  isChecked?: boolean // 아코디언 전체 선택 여부
+  contents?: Array<VCheckboxAccordionContentProps> // 아코디언 하위 목록
+}
+
+// props
+const props = withDefaults(defineProps<VCheckboxAccordionProps>(), {
+  modelValue: false,
+  isChecked: false
+})
+
+// emit
+const emit = defineEmits(["change:all", "change:content"])
+
+// reactive
+const _modelValue = props.modelValue ? ref([0]) : ref([])
+
+const _isChecked = ref(props.isChecked)
+
+// Props 받을 때 전체선택 여부 확인해서 세팅
+const _contents = reactive({
+  contentArr: props.contents
+})
+
+_contents.contentArr?.forEach(data => _isChecked.value && (data.checked = _isChecked.value))
+
+// 전체 체크 선택에 따라 아코디언 하위 항목 변경
+const onClickCheckAll = (event:MouseEvent, checked: boolean) => {
+  _contents.contentArr?.forEach(data => (data.checked = checked))
+}
+
+// watcher
+watch(
+  () => props.isChecked,
+  () => {
+    _isChecked.value = props.isChecked
+    _contents.contentArr?.forEach(data => (data.checked = _isChecked.value))
+  }
+)
+
+watch(
+  () => props.modelValue,
+  () => {
+    _modelValue.value = props.modelValue ? [0] : []
+  }
+)
+
+// 하위 항목 전체 선택 여부에 따라 헤더 체크 여부 변경
+watch(_contents, () => {
+  if(_contents.contentArr?.filter(data => data.checked).length === _contents.contentArr?.length) {
+    !_isChecked.value && (_isChecked.value = true)
+  } else {
+    _isChecked.value && (_isChecked.value = false)
+  }
+})
+
+watch(_isChecked, () => {
+  emit("change:all", _isChecked.value, props.id)
+})
+
+// method
+const onClickCheck = (event:MouseEvent, index: number, checked: boolean, id?: string) => {
+  emit("change:content", index, checked, id)
+}
 </script>
 
 <style lang="scss">
@@ -35,10 +100,6 @@
   & .v-expansion-panel {
     color: initial;
     border: 1px solid transparent;
-
-    &.v-expansion-panel--active {
-      border-color: #e5e5e5;
-    }
 
     & .v-expansion-panel-title {
       padding: 16px 20px;
@@ -53,9 +114,22 @@
         font-weight: bold;
       }
     }
+  }
 
-    & .v-expansion-panel-text__wrapper {
-      padding-top: 8px;
+  & .v-expansion-panel-text__wrapper {
+    padding: 0 0 5px 20px;
+
+    & .v-label {
+      font-weight: 800;
+    }
+
+    & .v-input__details {
+      display: none;
+    }
+
+    & .item__detail {
+      padding: 0 20px 5px 20px;
+      text-align: left;
     }
   }
 }
--- src/views/Example/CheckboxAccordionView.vue
+++ src/views/Example/CheckboxAccordionView.vue
@@ -1,8 +1,42 @@
 <template>
   <h2>CheckboxAccordion</h2>
-  <VCheckboxAccordion />
+  <div class="accordion_container">
+    <VCheckboxAccordion v-bind="accordionState" />
+  </div>
 </template>
 
 <script setup lang="ts">
 import VCheckboxAccordion from '@/components/common/VCheckboxAccordion.vue';
+import {reactive} from "vue"
+
+const accordionState = reactive({
+  titleText: "전체동의",
+  modelValue: true,
+  isChecked: true,
+  contents: [
+    {
+      id: "agree_1",
+      checkboxLabel: "[필수] 개인정보 수집, 이용 동의",
+      checked: true,
+      detail: `<p>필수 개인정보 동의 사항으로써 아래 내용을 반드시 참고하시기 바랍니다.</p>
+               <p>vue에 대해서 알아보고 vuetify를 활용할 수 있도록 합시다.</p>`
+    },
+    {
+      id: "agree_2",
+      checkboxLabel: "[필수] 개인정보 수집, 이용 동의",
+      checked: true,
+      detail: `<p>필수 개인정보 동의 사항으로써 아래 내용을 반드시 참고하시기 바랍니다.</p>
+               <p>vue에 대해서 알아보고 vuetify를 활용할 수 있도록 합시다.</p>`
+    },
+  ]
+})
 </script>
+
+<style lang="scss">
+.accordion_container {
+  width: 90%;
+  margin: auto;
+  padding-top: 10px;
+  padding-bottom: 10px;
+}
+</style>
